f2f3eba2ee2aba52573b3f3a69acdee852b6399b
egorgrebennykov-del <egor.grebennykov@gmail.com>
2025-11-01 13:48:45 +0200

    Class Storage
    Create a class Storage. It should have the following properties, while the constructor should only receive a capacity:
    
    · capacity – a number that decreases when adding a given quantity of products to storage
    
    · storage – list of products (object). Each product should have:
    
    o name - a string
    
    o price – a number (price is for a single piece of product)
    
    o quantity – a number
    
    · totalCost – the sum of the cost of the products
    
    The class should also have the following methods:
    
    · addProduct – a function that receives a product and adds it to the storage
    
    · getProcuts – a function that returns all the products in storage in JSON format, each on a new line
    
    Paste only the class Storage in judge (Note: all names should be as described)

---
+++ Class Storage.js
@@ -0,0 +1,40 @@
+class Storage{
+    constructor(capacity){
+        this.capacity = capacity;
+        this.storage = [];
+        this.totalCost = 0;
+    }
+
+    addProduct(productObj)
+    {
+        this.storage.push(productObj);
+        this.capacity -= productObj.quantity;
+        this.totalCost += productObj.price * productObj.quantity;
+    }
+
+    getProducts()
+    {
+        let productJsons = [];
+
+        for(let productObj of this.storage)
+        {
+            productJsons.push(JSON.stringify(productObj));
+        }
+
+        return productJsons.join('\n');
+    }
+}
+
+let productOne = {name: 'Cucamber',
+price: 1.50, quantity: 15};
+let productTwo = {name: 'Tomato',
+price: 0.90, quantity: 25};
+let productThree = {name: 'Bread',
+price: 1.10, quantity: 8};
+let storage = new Storage(50);
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+storage.addProduct(productThree);
+console.log(storage.getProducts());
+console.log(storage.capacity);
+console.log(storage.totalCost);
